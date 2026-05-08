c02d943713bcb0fa32ba43230fbf58198fa62662
Avaya Shrestha <90096324+avayashrestha01@users.noreply.github.com>
2026-05-08 18:03:39 +0545

    TEst

--- Lesson 1/script.js
+++ Lesson 1/script.js
@@ -1,1 +1,15 @@
-alert("Hello World")
+// alert("Hello World")
+
+// console.log("Hey I am a code!")
+
+
+var a = prompt("Enter your number!")
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer?") 
+
+if(isTrue){
+    console.log("Computer is blasting")
+}
+
+else{
+    console.log("Computer isn't blasting")
+}

2df5a225bb4a885e82496c31aaaa40ec340bf75d
Avaya Shrestha <90096324+avayashrestha01@users.noreply.github.com>
2026-05-11 15:56:13 +0545

    booost

--- Lesson 1/practice.js
+++ Lesson 1/practice.js
@@ -2,8 +2,24 @@ var a = 0;
 
 
 function fn() {
-    a ++;
+    a++;
     b = " times";
 
-    document.getElementById('output').innerText = a+b;
+    document.getElementById('output').innerText = a + b;
+
+    if (a >= 100) {
+        a += 1;
+    }
+
+    if (a >= 250) {
+        a += 2;
+    }
+
+    if (a >= 500) {
+        a += 16;
+    }
+
+    if (a >= 5000) {
+        a += 180;
+    }
 }

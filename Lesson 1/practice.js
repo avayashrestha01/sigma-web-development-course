af3ac927af2c1fcd010c81298b93a1194e6c2357
Avaya Shrestha <90096324+avayashrestha01@users.noreply.github.com>
2026-05-11 14:08:14 +0545

    minot

--- Lesson 1/practice.js
+++ Lesson 1/practice.js
@@ -4,8 +4,15 @@ var a = 0;
 function fn() {
     a ++;
 
-
-
-    
     document.getElementById('output').innerText = a;
 }
+
+
+function changeStyle(){
+    let box = document.getElementById("output");
+
+    output.style.color = yellow;
+    output.style.scale = 1.1;
+}
+
+document.getElementById("clickme").onclick = changeStyle();

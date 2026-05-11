d4425d252f7b5019d9285c3574610eeff1b2f088
Avaya Shrestha <90096324+avayashrestha01@users.noreply.github.com>
2026-05-11 12:59:37 +0545

    Update script.js

--- Lesson 1/script.js
+++ Lesson 1/script.js
@@ -3,16 +3,20 @@
 // console.log("Hey I am a code!")
 
 
-var a = prompt("Enter your number!")
-var isTrue = confirm("Are you sure you want to leave this page and blast your computer?") 
+// var a = prompt("Enter your number!")
+// var isTrue = confirm("Are you sure you want to leave this page and blast your computer?") 
 
-if(isTrue){
-    console.log("Computer is blasting")
-}
+// if(isTrue){
+//     console.log("Computer is blasting")
+// }
 
-else{
-    console.log("Computer isn't blasting")
-}
+// else{
+//     console.log("Computer isn't blasting")
+// }
 
 
 
+// document.title("Hello World")
+
+
+// document.body.style.backgroundColor = "red";

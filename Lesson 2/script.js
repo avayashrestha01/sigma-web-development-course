26e3ea5edc754324465a7551e5a2ddd7d5db68bd
Avaya Shrestha <90096324+avayashrestha01@users.noreply.github.com>
2026-05-15 09:52:19 +0545

    Update script.js

--- Lesson 2/script.js
+++ Lesson 2/script.js
@@ -1,8 +1,44 @@
-console.log('Text')
+let a = 10;
+a += 2;
 
-var a = 5;
-var b = 6;
-var c = "Avaya";
 
-console.log(a + b + 8)
-console.log(typeof a, typeof b, typeof c)
+{
+    let a = 100;
+    console.log(a)
+}
+
+console.log(a)
+
+
+// var b = 10;
+
+// {
+//     var b = 100;
+//     console.log(b)
+// }
+
+// console.log(b)
+
+
+
+
+let x = "Avaya Shrestha";
+let y = 16;
+let z = 3.55;
+const c = true;
+let q = undefined;
+let r = null;
+
+
+console.log(x, y, z, c, q, r)
+console.log(typeof x, typeof y, typeof z, typeof c, typeof q, typeof r)
+
+
+let detail = {
+    name: "Avaya Shrestha",
+    age: 16,
+    education = "10 Completed",
+    DOB = "2010-11-12",
+}
+
+console.log(detail)

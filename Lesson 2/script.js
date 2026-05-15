b206072b706d0044d22c775aa4fbc64016d8d365
Avaya Shrestha <90096324+avayashrestha01@users.noreply.github.com>
2026-05-15 09:31:57 +0545

    Update script.js

--- Lesson 2/script.js
+++ Lesson 2/script.js
@@ -1,4 +1,8 @@
-var a = 1000;
-let b = "I a";
+console.log('Text')
 
-document.getElementById('output').innerText = a + b
+var a = 5;
+var b = 6;
+var c = "Avaya";
+
+console.log(a + b + 8)
+console.log(typeof a, typeof b, typeof c)

9747038bb42cb063e0b0fd91b05577d0013c06d2
Avaya Shrestha <90096324+avayashrestha01@users.noreply.github.com>
2026-05-25 13:21:08 +0545

    Update script.js

--- Lesson 4/script.js
+++ Lesson 4/script.js
@@ -1,10 +1,23 @@
 console.log('Loop Tutorial!')
 
+function line(){
+    let gap = 0;
+    
+    while (gap<1){
+        console.log("")
+        gap++;
+    }
+}
+
+line()
+
 let a = 1;
 
 for (let i = 0; i < 10; i++) {
     console.log(a + i);
 }
+
+line()
 
 let det = {
     name: "Avaya Shrestha",
@@ -17,7 +30,16 @@ for (const key in det){
     console.log(key + ' : ' + element)
 }
 
+line()
+
 for (const char of "Avaya") {
     console.log(char)
 }
 
+line()
+
+let i = 6;
+
+while (i<8) {
+    console.log(i++);
+}

369b323e1e80ec10f150e401a23aafbdb168c5b9
Avaya Shrestha <90096324+avayashrestha01@users.noreply.github.com>
2026-05-25 13:07:38 +0545

    Update script.js

--- Lesson 4/script.js
+++ Lesson 4/script.js
@@ -2,6 +2,21 @@ console.log('Loop Tutorial!')
 
 let a = 1;
 
-for (let i = 0; i < 1000000; i++) {
+for (let i = 0; i < 10; i++) {
     console.log(a + i);
+}
+
+let det = {
+    name: "Avaya Shrestha",
+    role: "Student",
+    company: null
+}
+
+for (const key in det){
+    const element = det[key];
+    console.log(key + ' : ' + element)
+}
+
+for (const element of object) {
+    
 }

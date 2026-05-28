0a07175018c38e89a3bfb089cd2bfc955541982d
Avaya Shrestha <90096324+avayashrestha01@users.noreply.github.com>
2026-05-28 16:35:28 +0545

    Lesson 7

---
+++ Lesson 7/script.js
@@ -0,0 +1,25 @@
+let name = "Avaya";
+
+console.log(name);
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+console.log(name[3]);
+console.log(name[4]);
+
+
+console.log(name.length)
+
+let caste = "Shrestha";
+
+console.log("His name is", name + " " + caste);
+console.log(`His name is ${name} ${caste}`);
+
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.length);
+console.log(name.slice(1, 4));
+console.log(name.replace("a", "aa"));
+console.log(name.concat(" ", caste, " is a legend"));
+
+console.log(name)

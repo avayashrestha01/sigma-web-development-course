6335052da51141b94652992699814a798486096a
Avaya Shrestha <90096324+avayashrestha01@users.noreply.github.com>
2026-05-28 14:14:43 +0545

    Lesson 6

---
+++ Lesson 6/script.js
@@ -0,0 +1,17 @@
+let name = "Avaya";
+
+console.log(name);
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+console.log(name[3]);
+console.log(name[4]);
+
+
+console.log(name.length)
+
+let caste = "Shrestha";
+
+console.log("His name is", name + " " + caste);
+console.log(`His name is ${name} ${caste}`);
+

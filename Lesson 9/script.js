993abee9554252b5b11c4587dab8454ae9f31296
Avaya Shrestha <90096324+avayashrestha01@users.noreply.github.com>
2026-06-01 12:44:57 +0545

    Update script.js

--- Lesson 9/script.js
+++ Lesson 9/script.js
@@ -1,1 +1,18 @@
 document.title = "JavaScript DOM - Child, parents and sibling";
+
+
+let cont = document.body.childNodes[1];
+
+
+
+console.log(cont.childNodes);
+console.log(cont.firstElementChild);
+console.log(cont.parentElement);
+console.log(cont.children);
+console.log(cont.children[0]);
+console.log(cont.children[1]);
+console.log(cont.children[2]);
+console.log(cont.children[3]);
+console.log(cont.children[3].nextElementSibling);
+console.log(cont.children[3].previousElementSibling);
+console.log(cont.children[3].previousSibling);
